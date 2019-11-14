a4a1619ebc16d65d39d45fe00c4105b0d75a9639
Arex360 <49303647+Arex360@users.noreply.github.com>
2019-11-14 10:37:34 +0500

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,16 +1,13 @@
 Arr = []
-ArrRange = int(input("Enter the range: "))
+ArrRange = int(input("Enter the range"))
 for i in range(ArrRange):
-  temp = int(input("Enter the integer{}".format(i+1)))
-  Arr.append(temp)
+    num = int(input("Enter number{} ".format(i+1)))
+    Arr.append(num)
 for number in Arr:
-  for j in range(2,number):
-    if(number % i == 0):
-      print("{} is not a prime number".format(number))
-      break
-  if number == 1:
-    print("1 is not a prime number")
-  else:
-    print("{} is a prime number".format(number))
-             
-
+    for i in range(2,number):
+        if(number % i == 0):
+            print("{} is not prime".format(number))
+            break
+    else:
+        print("{} is prime".format(number))
+        
